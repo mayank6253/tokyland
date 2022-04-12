50c8c1b0ec83f4312f40225901508a8f53eb6a3c
mayank6253 <89696473+mayank6253@users.noreply.github.com>
2022-04-12 18:38:18 +0530

    reveal-section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Fade } from "react-reveal";
 import image1 from "./images/image1.png";
 import image2 from "./images/image2.png";
+import image3 from "./images/image3.png";
 import Services from "./pages/Services";
 
 export default function Main() {
@@ -11,7 +13,7 @@ export default function Main() {
           <div className="row">
             <div className="col-12 col-lg-5 col-md-12">
               <div className="heading-container">
-                <h1 className="text-light fs-2 w-75">
+                <h1 className="text-light fs-1">
                   Decenteralized Crypto Trading Platform
                 </h1>
                 <p className="my-3 text-light">
@@ -19,10 +21,10 @@ export default function Main() {
                   Eveniet dolorem blanditiis ad perferendis, labore delectus
                   dolor sit amet, adipisicing elit. Eveniet.
                 </p>
-                <button className="buttons border border-2 border-light text-light my-3 rounded-pill fs-6 fw-bold">
+                <button className="buttons border border-1 border-light text-light my-3 rounded-pill fs-6 fw-bold">
                   WHITEPAPER
                 </button>
-                <button className="buttons border border-2 border-light text-light my-3 mx-3 rounded-pill fs-6 fw-bold">
+                <button className="buttons border border-1 border-light text-light my-3 mx-3 rounded-pill fs-6 fw-bold">
                   BUY TOKEN
                 </button>
               </div>
@@ -41,47 +43,101 @@ export default function Main() {
           <div className="row">
             <div className="col-12 col-lg-6 offset-lg-0 col-md-12 no-padding-left">
               <div className="image">
-                <img src={image2} alt="" className="img2" />
+                <Fade bottom>
+                  <img src={image2} alt="" className="img2" />
+                </Fade>
               </div>
             </div>
             <div className="col-12 col-lg-6 offset-lg-0">
-              <div className="content-container">
-                <span className="gradient-text ">
-                  Decentralized Trading Platform
-                </span>
-                <h2 className="text-light my-3 fs-1">
-                  Connect blockchain to the real world and start crypto tading.
-                </h2>
-                <p
-                  style={{
-                    color: "#81819C",
-                    marginTop: "30px",
-                    fontSize: "18px",
-                  }}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  at dictum risus, non suscipit arcu. Quisque aliquam posuere
-                  tortor, sit amet convallis nunc scelerisque in.
-                </p>
-                <p
-                  style={{
-                    color: "#81819C",
-                    marginTop: "40px",
-                    fontSize: "18px",
-                  }}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Suscipit ipsa ut quasi adipisci voluptates, voluptatibus
-                  aliquid alias beatae reprehenderit incidunt iusto laboriosam.
-                </p>
-                <button className="read-more border border-2 border-light text-light my-3 rounded-pill">READ MORE</button>
-              </div>
+              <Fade bottom>
+                <div className="content-container">
+                  <span className="gradient-text ">
+                    Decentralized Trading Platform
+                  </span>
+                  <h4 className="blockchain text-light my-3">
+                    Connect blockchain to the real world and start crypto
+                    tading.
+                  </h4>
+                  <p
+                    style={{
+                      color: "#81819C",
+                      marginTop: "30px",
+                      fontSize: "18px",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis at dictum risus, non suscipit arcu. Quisque aliquam
+                    posuere tortor, sit amet convallis nunc scelerisque in.
+                  </p>
+                  <p
+                    style={{
+                      color: "#81819C",
+                      marginTop: "40px",
+                      fontSize: "18px",
+                    }}
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Suscipit ipsa ut quasi adipisci voluptates, voluptatibus
+                    aliquid alias beatae reprehenderit incidunt iusto
+                    laboriosam.
+                  </p>
+                  <button className="read-more border border-1 border-light text-light my-3 rounded-pill">
+                    READ MORE
+                  </button>
+                </div>
+              </Fade>
             </div>
           </div>
         </div>
       </section>
+
       <div id="services">
         <Services />
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="decentralized-content">
+              <span className="gradient-text blue fw-bolder">
+                Decentralized Trading Platform
+              </span>
+              <h4 className="blockchain text-light my-3">
+                Connect blockchain to the real world and start crypto tading.
+              </h4>
+              <p
+                style={{
+                  color: "#81819C",
+                  marginTop: "30px",
+                  fontSize: "18px",
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
+                dictum risus, non suscipit arcu. Quisque aliquam posuere tortor,
+                sit amet convallis nunc scelerisque in.
+              </p>
+              <p
+                style={{
+                  color: "#81819C",
+                  marginTop: "40px",
+                  fontSize: "18px",
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Suscipit ipsa ut quasi adipisci voluptates, voluptatibus aliquid
+                alias beatae reprehenderit incidunt iusto laboriosam.
+              </p>
+              <button className="read-more border border-1 border-light text-light my-3 rounded-pill">
+                READ MORE
+              </button>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="image">
+              <img src={image3} alt="" className="vert-move" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,83 +1,227 @@
 import React from "react";
 
 export default function Services() {
-  const serviceInfo = [
-    {
-      number: "1",
-      title: "Creative Chart Modules",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem ipsum quam, maximus ut accumsan.",
-    },
-    {
-      number: "2",
-      title: "Adaptive Social Assistant",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem ipsum quam, maximus ut accumsan.",
-    },
-    {
-      number: "3",
-      title: "Ecommerce Embedded",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem ipsum quam, maximus ut accumsan.",
-    },
-    {
-      number: "4",
-      title: "Ecommerce Embedded",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem ipsum quam, maximus ut accumsan.",
-    },
-    {
-      number: "5",
-      title: "Adaptive Social Assistant",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem ipsum quam, maximus ut accumsan.",
-    },
-    {
-      number: "6",
-      title: "Creative Chart Modules",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem ipsum quam, maximus ut accumsan.",
-    },
-  ];
-  const renderService = (serviceInfo, index) => {
-    return (
-      <>
-        <div className=" col-lg-4 col-md-6 grid-items my-5" key={index}>
-          <div className="card" style={{ width: "18rem" }}>
-            <div className="card-body">
-              <h5 className="card-title text-center text-light">
-                {serviceInfo.title}
-              </h5>
-              <p className="card-text text-light text-center" style={{marginTop:"20px"}}>
-                {serviceInfo.description}
-              </p>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
   return (
-    <div>
-      <section>
-        <div id="#services">
-          <div className="container">
-            <div className="service-content text-center">
-              <span className="gradient-text my-2">Our Services</span>
-              <h2 className="text-light">What We Speciaized In</h2>
-              <p style={{ color: "#81819C", marginTop: "20px" }}>
-                Our ICO Template Will Be A Perfect Platform For Presenting Your
-                Ico Launch.
-                <br />
-                This Landing Page Comes In Great And Clean Design
-              </p>
-            </div>
+    <>
+      <div>
+        <section>
+          <div id="#services">
+            <div className="container">
+              <div className="service-content text-center">
+                <span className="gradient-text my-2">Our Services</span>
+                <h2 className="text-light">What We Speciaized In</h2>
+                <p style={{ color: "#81819C", marginTop: "20px" }}>
+                  Our ICO Template Will Be A Perfect Platform For Presenting
+                  Your Ico Launch.
+                  <br />
+                  This Landing Page Comes In Great And Clean Design
+                </p>
+              </div>
 
-            <div className="row" style={{background:"#100f43", maxWidth:"1280px", margin:"auto"}}>{serviceInfo.map(renderService)}</div>
+              <div
+                className="row"
+                style={{
+                  background:"#100F43",
+                  maxWidth: "1280px",
+                  margin: " 70px auto",
+                }}
+              >
+                <div className=" col-lg-4 col-md-6 grid-items">
+                  <div
+                    className="card position-relative"
+                    style={{ width: "18rem" }}
+                  >
+                    <div className="contribution-c-wapper has-arrow-right-lg has-arrow-top-md has-arrow-right-md has-arrow-down-sm">
+                      <div className="card-body">
+                        <div className="numbers">
+                          <span className="gradient-t orange">1</span>
+                        </div>
+                        <h5
+                          className="card-title text-center text-light"
+                          style={{ marginTop: "25px" }}
+                        >
+                          Creative Chart Modules
+                        </h5>
+                        <p
+                          className="card-text text-light text-center"
+                          style={{ marginTop: "10px" }}
+                        >
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nulla neque quam, maxi ut accumsan ut, posuere
+                          sit Lorem ipsum quam, maximus ut accumsan.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className=" col-lg-4 col-md-6 grid-items">
+                  <div
+                    className="card position-relative"
+                    style={{ width: "18rem" }}
+                  >
+                    <div className="contribution-c-wapper has-arrow-right-lg has-arrow-top-md has-arrow-right-md has-arrow-down-sm">
+                      <div className="card-body">
+                        <div className="numbers">
+                          <span className="gradient-t pink">2</span>
+                        </div>
+                        <h5
+                          className="card-title text-center text-light"
+                          style={{ marginTop: "25px" }}
+                        >
+                          Adaptive Social Assistant
+                        </h5>
+                        <p
+                          className="card-text text-light text-center"
+                          style={{ marginTop: "10px" }}
+                        >
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nulla neque quam, maxi ut accumsan ut, posuere
+                          sit Lorem ipsum quam, maximus ut accumsan.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                 <div className=" col-lg-4 col-md-6 grid-items">
+                  <div
+                    className="card position-relative"
+                    style={{ width: "18rem" }}
+                  >
+                    <div className="card-body">
+                      <div className="numbers">
+                        <span className="gradient-t blue">3</span>
+                      </div>
+                      <h5
+                        className="card-title text-center text-light"
+                        style={{ marginTop: "25px" }}
+                      >
+                        Ecommerce Embedded
+                      </h5>
+                      <p
+                        className="card-text text-light text-center"
+                        style={{ marginTop: "10px" }}
+                      >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem
+                        ipsum quam, maximus ut accumsan.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4  text-center">
+                  <img
+                    src="	https://ico1.netlify.app/static/media/join-bottom.27d84c79.svg"
+                    alt=""
+                    draggable="false"
+                    className="arrow-img rotated"
+                  />
+                </div>
+                <div className="col-lg-4 text-center ">
+                  <div
+                    className="dream-dots justify-content-center fadeInUp aos-init aos-animate"
+                    data-aos="fade-up"
+                    data-aos-delay="200"
+                  >
+                    <span className="gradient-text">Join us</span>
+                    <h2 className="text-light">Contribution Cycle</h2>
+                  </div>
+                </div>
+                <div className="col-lg-4  text-center">
+                  <img
+                    src="	https://ico1.netlify.app/static/media/join-bottom.27d84c79.svg"
+                    alt=""
+                    draggable="false"
+                    className="arrow-img"
+                  />
+                </div>
+                <div className=" col-lg-4 col-md-6 grid-items">
+                  <div
+                    className="card position-relative"
+                    style={{ width: "18rem" }}
+                  >
+                    <div className="contribution-c-wapper has-arrow-left-lg has-arrow-top-md has-arrow-left-md has-arrow-down-sm">
+                      <div className="card-body">
+                        <div className="numbers">
+                          <span className="gradient-t green">6</span>
+                        </div>
+                        <h5
+                          className="card-title text-center text-light"
+                          style={{ marginTop: "25px" }}
+                        >
+                          Creative Chart Modules
+                        </h5>
+                        <p
+                          className="card-text text-light text-center"
+                          style={{ marginTop: "10px" }}
+                        >
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nulla neque quam, maxi ut accumsan ut, posuere
+                          sit Lorem ipsum quam, maximus ut accumsan.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className=" col-lg-4 col-md-6 grid-items">
+                  <div
+                    className="card position-relative"
+                    style={{ width: "18rem" }}
+                  >
+                    <div className="contribution-c-wapper has-arrow-left-lg has-arrow-top-md has-arrow-left-md has-arrow-down-sm">
+                      <div className="card-body">
+                        <div className="numbers">
+                          <span className="gradient-t green">5</span>
+                        </div>
+                        <h5
+                          className="card-title text-center text-light"
+                          style={{ marginTop: "25px" }}
+                        >
+                          Adaptive Social Assistant
+                        </h5>
+                        <p
+                          className="card-text text-light text-center"
+                          style={{ marginTop: "10px" }}
+                        >
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Nulla neque quam, maxi ut accumsan ut, posuere
+                          sit Lorem ipsum quam, maximus ut accumsan.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className=" col-lg-4 col-md-6 grid-items">
+                  <div
+                    className="card position-relative"
+                    style={{ width: "18rem" }}
+                  >
+                    <div className="card-body">
+                      <div className="numbers">
+                        <span className="gradient-t pink">4</span>
+                      </div>
+                      <h5
+                        className="card-title text-center text-light"
+                        style={{ marginTop: "25px" }}
+                      >
+                        Ecommerce Embedded
+                      </h5>
+                      <p
+                        className="card-text text-light text-center"
+                        style={{ marginTop: "10px" }}
+                      >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nulla neque quam, maxi ut accumsan ut, posuere sit Lorem
+                        ipsum quam, maximus ut accumsan.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 }
